2673996cabd5390e66f790c8cf93df95313efd5c
blokongithub <blokongithub@gmail.com>
2024-06-11 20:36:31 +0100

    updated commands
    changed /kitroom to open the kitroom instead of the menu

--- src/main/java/blokplugins/kitroom/commands/CommandKit.java
+++ src/main/java/blokplugins/kitroom/commands/CommandKit.java
@@ -6,7 +6,7 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-public class kit implements CommandExecutor {
+public class CommandKit implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String s, String[] strings) {
         if(!(sender instanceof Player)) {
