b3c7b4ecd4bd5f94ab766494f93c04ca60437fdd
blokongithub <blokongithub@gmail.com>
2024-06-11 16:42:55 +0100

    switch to maven

--- src/main/java/blokplugins/kitroom/Kitroom.java
+++ src/main/java/blokplugins/kitroom/Kitroom.java
@@ -2,13 +2,12 @@ package blokplugins.kitroom;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.io.Console;
-
 public final class Kitroom extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        System.out.println("kitroom enabled");
+        // Plugin startup logic
+
     }
 
     @Override
