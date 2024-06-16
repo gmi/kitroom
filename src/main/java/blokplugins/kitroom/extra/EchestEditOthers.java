704f2569d8d04fe32d93d4a7bf7628ddc4d70fe6
blokongithub <blokongithub@gmail.com>
2024-06-16 08:05:18 +0100

    kit echest holder

---
+++ src/main/java/blokplugins/kitroom/extra/EchestEditOthers.java
@@ -0,0 +1,12 @@
+package blokplugins.kitroom.extra;
+
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryHolder;
+
+public class EchestEditOthers implements InventoryHolder {
+
+    @Override
+    public Inventory getInventory() {
+        return null;
+    }
+}
