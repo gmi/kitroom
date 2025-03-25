c0b7872bf33be79c055cdd18c71d23a98fe0cb1d
vulger <blokongithub@gmail.com>
2025-03-25 21:53:46 +0000

    v2 reset

--- src/main/java/blokplugins/kitroom/Kitroom.java
+++ src/main/java/blokplugins/kitroom/Kitroom.java
@@ -1,68 +1,15 @@
 package blokplugins.kitroom;
 
-import blokplugins.kitroom.commands.*;
-import blokplugins.kitroom.database.PointsDatabase;
-import blokplugins.kitroom.extra.DBstartup;
-import blokplugins.kitroom.extra.InventorySerializations;
-import blokplugins.kitroom.listners.*;
-import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.sql.SQLException;
-import java.util.Arrays;
-import java.util.List;
 
 public final class Kitroom extends JavaPlugin {
-    private PointsDatabase pointsDatabase;
-    private InventorySerializations inventorySerializer;
-
     @Override
     public void onEnable() {
-        try {
-            if (!getDataFolder().exists()) {
-                getDataFolder().mkdirs();
-            }
-            pointsDatabase = new PointsDatabase(getDataFolder().getAbsolutePath() + "/kits.db");
-            inventorySerializer = new InventorySerializations(this, pointsDatabase);
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-            getLogger().severe("Failed to connect to database: " + ex.getMessage());
-            Bukkit.getPluginManager().disablePlugin(this);
-            return;
-        }
-        List<String> filePaths = Arrays.asList("vanillapvp", "diamoncrystal", "potions", "armory", "axe");
-        DBstartup dBstartup = new DBstartup(getLogger(), getDataFolder(), pointsDatabase);
-        try {
-            dBstartup.defaultDB(filePaths);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        this.getCommand("kitview").setExecutor(new CommandKitView());
-        this.getCommand("kitadmin").setExecutor(new CommandKitAdmin(inventorySerializer, pointsDatabase));
-        this.getCommand("kit").setExecutor(new CommandKit());
-        this.getCommand("kitroom").setExecutor(new CommandKitroom(inventorySerializer, pointsDatabase));
-        getServer().getPluginManager().registerEvents(new kitadminlistner(inventorySerializer, pointsDatabase), this);
-        getServer().getPluginManager().registerEvents(new kitmenuotherslistner(inventorySerializer, pointsDatabase), this);
-        getServer().getPluginManager().registerEvents(new mainmenulistner(inventorySerializer, pointsDatabase), this);
-        getServer().getPluginManager().registerEvents(new editkitlistnet(inventorySerializer, pointsDatabase), this);
-        getServer().getPluginManager().registerEvents(new kitroomitemslistner(inventorySerializer, pointsDatabase), this);
-        getServer().getPluginManager().registerEvents(new echestlistner(inventorySerializer, pointsDatabase), this);
-        for (int i = 1; i <= 9; i++) {
-            this.getCommand("k" + i).setExecutor(new CommandKitLoader(inventorySerializer, pointsDatabase));
-        }
-        for (int i = 1; i <= 9; i++) {
-            this.getCommand("ec" + i).setExecutor(new CommandEchestLoader(inventorySerializer, pointsDatabase));
-        }
     }
 
     @Override
     public void onDisable() {
-        try {
-            if (pointsDatabase != null) {
-                pointsDatabase.closeConnection();
-            }
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-        }
     }
 }
